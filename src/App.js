468cb5a13392615071a065fc72aa4542dcc7f52f
kjbaumann <baumannkonrad@outlook.de>
2024-08-20 21:18:58 +0200

    Implements main routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Work from './pages/Work';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Work.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Work() {
+  return <div>Our Work</div>;
+}
+
+export default Work;
